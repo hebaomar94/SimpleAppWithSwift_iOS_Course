e157201b9548cec82f505eac4adf25b2b0f79efa
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-11-06 08:55:27 +0200

    Add files via upload

---
+++ taskapp10/SimpleAppWithSwift_iOS_Course-main/TaskApp/My_Tasks/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extensions.swift
+//  My_Tasks
+//
+//  Created by Heba Omar94 on 04/11/2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(r red: Int, g green: Int, b blue: Int, a: CGFloat = 1.0) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: a)
+    }
+    
+    convenience init(hex: Int, a: CGFloat = 1.0) {
+        self.init(r: (hex >> 16) & 0xFF, g: (hex >> 8) & 0xFF, b: hex & 0xFF, a: a)
+    }
+}
